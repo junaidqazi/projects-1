1370cf5f4df093a1fd4a06295619745522894505
huseinzol05 <husein.zol05@gmail.com>
2022-05-11 17:17:58 +0800

    test torch dataset

--- malaysia_ai_projects/malay_vits/utils.py
+++ malaysia_ai_projects/malay_vits/utils.py
@@ -135,9 +135,9 @@ def load_wav_to_torch(full_path):
     return torch.FloatTensor(data.astype(np.float32)), sampling_rate
 
 
-def load_filepaths_and_text(filename, split="|"):
-    with open(filename, encoding='utf-8') as f:
-        filepaths_and_text = [line.strip().split(split) for line in f]
+def load_filepaths_and_text(filename):
+    with open(filename) as fopen:
+        return json.load(fopen)
     return filepaths_and_text
 
 
@@ -189,3 +189,35 @@ def get_hparams_from_file(config_path):
 
     hparams = HParams(**config)
     return hparams
+
+
+class HParams():
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            if type(v) == dict:
+                v = HParams(**v)
+            self[k] = v
+
+    def keys(self):
+        return self.__dict__.keys()
+
+    def items(self):
+        return self.__dict__.items()
+
+    def values(self):
+        return self.__dict__.values()
+
+    def __len__(self):
+        return len(self.__dict__)
+
+    def __getitem__(self, key):
+        return getattr(self, key)
+
+    def __setitem__(self, key, value):
+        return setattr(self, key, value)
+
+    def __contains__(self, key):
+        return key in self.__dict__
+
+    def __repr__(self):
+        return self.__dict__.__repr__()
